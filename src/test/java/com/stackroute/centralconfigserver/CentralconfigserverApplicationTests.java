da3d2284a63e2048119f4c88103c56dc4c936bef
marypeterdan <89521160+marypeterdan@users.noreply.github.com>
2021-08-25 16:57:07 +0530

    Add files via upload

---
+++ src/test/java/com/stackroute/centralconfigserver/CentralconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.centralconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
